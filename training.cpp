1c95c7ffcc01b1d410c22c09de48416074f1f1f7
Artur Korsik <funomegan4ik@gmail.com>
2019-04-18 02:24:27 +0300

    some changes in parse

--- training.cpp
+++ training.cpp
@@ -1,34 +1,50 @@
 #include <boost/spirit/include/qi.hpp>
+#include <boost/bind.hpp>
 #include <string>
-namespace qi = boost::spirit::qi;
-
+#include <vector>
 #include "IOperand.hpp"
 
-bool	checkLine(std::string &line)
+namespace qi = boost::spirit::qi;
+
+void	push(boost::fusion::vector<boost::variant<boost::fusion::vector<int, char>,
+		boost::fusion::vector<short, char>, boost::fusion::vector<float, char>,
+		boost::fusion::vector<double, char> >, boost::optional<std::vector<char, std::allocator<char> > >
+> x)
+{
+	std::cout << boost::fusion::at_c<0>(x) << std::endl;
+}
+bool	parseLine(std::string &line)
 {
 	std::string::iterator it = line.begin();
+
 	const auto char_number_ = [](const int &attr, auto &, bool &pass) {
 		pass = (attr > -129 && attr < 128);
-	};
+	}; // lambda to check that number in char limits
 
+
+	//Rule to parse supported data types
 	auto types = qi::lexeme[qi::lit("int8(") >> qi::int_[char_number_] >> qi::char_(')') |
-							qi::lit("int16(") >> qi::short_ >> qi::char_(')')  |
-							qi::lit("int32(") >> qi::int_ >> qi::char_(')')  |
-							qi::lit("float(") >> qi::float_ >> qi::char_(')')  |
-							qi::lit("double(") >> qi::double_ >> qi::char_(')') ];
+						 	qi::lit("int16(") >> qi::short_ >> qi::char_(')')  |
+						 	qi::lit("int32(") >> qi::int_ >> qi::char_(')')  |
+						 	qi::lit("float(") >> qi::float_ >> qi::char_(')')  |
+						 	qi::lit("double(") >> qi::double_ >> qi::char_(')') ];
+
+	auto comment = ';' >> *qi::char_; // Rule to parse commentary
 
 	return qi::phrase_parse(it, line.end(),
-								"push" >> types |
-								"pop" |
-								"dump" |
-								"assert" >> types |
-								"add" |
-								"sub" |
-								"mul" |
-								"div" |
-								"mod" |
-								"print" |
-								"exit",
+								comment |
+								("push" >> types >> -comment) [boost::bind(&push, _1)]|
+								"pop" >> -comment |
+								"dump" >> -comment |
+								"assert" >> types >> -comment |
+								"add" >> -comment |
+								"sub" >> -comment |
+								"mul" >> -comment |
+								"div" >> -comment |
+								"mod" >> -comment |
+								"print" >> -comment |
+								"exit" >> -comment |
+								"",
 								qi::space) && it == line.end();
 }
 
@@ -39,6 +55,6 @@ int main()
 	while (true)
 	{
 		std::getline(std::cin, line);
-		std::cout << checkLine(line) << std::endl;
+		std::cout << parseLine(line) << std::endl;
 	}
 }
